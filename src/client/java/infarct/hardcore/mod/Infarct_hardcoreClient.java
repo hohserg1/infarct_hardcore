3fd4e309fe4432d10fbe63c1d540a4b9a385f7bd
hohserg1 <hohserg1@yandex.ru>
2023-10-10 22:31:47 +0300

    Added state

--- src/client/java/infarct/hardcore/mod/Infarct_hardcoreClient.java
+++ src/client/java/infarct/hardcore/mod/Infarct_hardcoreClient.java
@@ -3,6 +3,9 @@ package infarct.hardcore.mod;
 import net.fabricmc.api.ClientModInitializer;
 
 public class Infarct_hardcoreClient implements ClientModInitializer {
+
+	public static boolean hardcoreDisguise = false;
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
