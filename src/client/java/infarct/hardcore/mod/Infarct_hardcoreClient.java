8d67d12854d250eb997a6e5dc6806c8a45aea2b4
hohserg1 <hohserg1@yandex.ru>
2023-10-10 22:03:54 +0300

    Init template

---
+++ src/client/java/infarct/hardcore/mod/Infarct_hardcoreClient.java
@@ -0,0 +1,10 @@
+package infarct.hardcore.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Infarct_hardcoreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
