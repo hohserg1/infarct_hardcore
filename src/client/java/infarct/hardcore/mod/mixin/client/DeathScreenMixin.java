edb57018a27c7511ae37a2a2e7ab09d9484a9802
hohserg1 <hohserg1@yandex.ru>
2023-10-18 17:07:11 +0300

    Simplify and fix button

--- src/client/java/infarct/hardcore/mod/mixin/client/DeathScreenMixin.java
+++ src/client/java/infarct/hardcore/mod/mixin/client/DeathScreenMixin.java
@@ -1,53 +1,48 @@
 package infarct.hardcore.mod.mixin.client;
 
 import infarct.hardcore.mod.Infarct_hardcoreClient;
-import net.minecraft.client.gui.Drawable;
+import java.util.List;
 import net.minecraft.client.gui.screen.DeathScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
-
 @Mixin(DeathScreen.class)
 public abstract class DeathScreenMixin {
+
     @Final
     @Mutable
-    @Shadow private boolean isHardcore;
-    @Shadow private int ticksSinceDeath;
+    @Shadow
+    private boolean isHardcore;
+    @Shadow
+    private int ticksSinceDeath;
 
     @Shadow
-    private List<Drawable> drawables;
-
-    @Shadow protected abstract void init();
-
-    @Shadow @Final private List<ButtonWidget> buttons;
-    @Unique
-    private boolean realHardCore = false;
+    @Final
+    private List<ButtonWidget> buttons;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void init(@Nullable Text message, boolean isHardcore, CallbackInfo ci) {
-        this.isHardcore = isHardcore || Infarct_hardcoreClient.hardcoreDisguise;
-        realHardCore = isHardcore;
-
-    }
-
-
-    @Inject(method = "init", at = @At("TAIL"))
-    private void init(CallbackInfo ci) {
-        this.buttons.get(0).active = !(Infarct_hardcoreClient.hardcoreDisguise && !realHardCore);
+        if (Infarct_hardcoreClient.hardcoreDisguise) {
+            this.isHardcore = true;
+        }
     }
 
     @Inject(method = "tick", at = @At("TAIL"))
     private void tick(CallbackInfo ci) {
-        if (this.ticksSinceDeath > 20 * 7) {
-            isHardcore = realHardCore;
-            init();
-            // this.drawables TODO
+        if (Infarct_hardcoreClient.hardcoreDisguise) {
+            if (this.ticksSinceDeath > 20 * 7) {
+                Text text = Text.translatable("deathScreen.respawn");
+                this.buttons.get(0).setMessage(text);
+                Infarct_hardcoreClient.hardcoreDisguise = false;
+            }
         }
     }
 }
